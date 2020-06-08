fe6862d1ac116a934b2f9d2cb2af954bce01a4dc
Kvria <trishakuria@gmail.com>
2020-06-08 23:22:35 +0300

    created url patterns for the remaining views

--- projecter/project/urls.py
+++ projecter/project/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     url('^$', views.home, name='home' ),
     url(r'^profile/', views.profile, name='profile'),
+    url(r'^post/new', views.post_new, name= 'new_post'),
+    url(r'^search/', views.search_users, name='search_users'),
 ]
