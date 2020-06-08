c164eefa08f9ce1ee234f2c765cae3253d223877
Kvria <trishakuria@gmail.com>
2020-06-08 15:53:25 +0300

    add post model

--- projecter/project/models.py
+++ projecter/project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length = 500)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
