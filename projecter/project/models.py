658e517853b39582bcc80c259c05d50cb3e0ac32
Kvria <trishakuria@gmail.com>
2020-06-08 22:04:27 +0300

    created home reaquest

--- projecter/project/models.py
+++ projecter/project/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from djangoratings.fields import RatingField
+
 
 # Create your models here.
 
@@ -9,6 +12,9 @@ class Post(models.Model):
     post_date = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
     
+    def __str__(self):
+        return self.image
+
     def save_image(self):
         self.save()
 
@@ -27,7 +33,29 @@ class Profile(models.Model):
     def save_profile(self):
         self.save()
 
+    def delete_profile(self):
+        self.delete()
+
     @classmethod
     def search_by_user(cls,search_term):
         users = User.objects.filter(username__icontains=search_term)
         return users
+
+class Rate(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='rate')
+    design = models.RatingField(range=10)
+    usability =models.RatingField(range=10)
+    content = models.RatingField(range=10)
+    objects = models.Manager()
+
+    def save_rating(self):
+        self.save()
+
+    def delete_rating(self):
+        self.delete()
+
+    @classmethod
+    def get_comments_on_image(cls, id):
+        the_comments = Comment.objects.filter(image__pk=id)
+        return the_comments
+   
--- projecter/project/views.py
+++ projecter/project/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post,Profile,User
 from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
+@login_required(login_url = "accounts/login")
+def home(request):
+    posts = Post.objects.all()
+    return render(request,'index.html',{"images":images})
+
