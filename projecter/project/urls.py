06456f9f8f71b22a81c8ea2df78495011dc4ad9b
Kvria <trishakuria@gmail.com>
2020-06-09 08:49:18 +0300

    created put and delete in views

--- projecter/project/urls.py
+++ projecter/project/urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
     url(r'^profile/', views.profile, name='profile'),
     url(r'^post/new', views.post_new, name= 'new_post'),
     url(r'^search/', views.search_users, name='search_users'),
-    url(r'^api/merch/$', views.MerchList.as_view())
+    url(r'^api/project/$', views.ProjectList.as_view()),
+    url(r'api/project/project-id/(?P<pk>[0-9]+)/$',
+        views.ProjectDescription.as_view())
 ]
--- projecter/project/views.py
+++ projecter/project/views.py
@@ -80,7 +80,7 @@ def search_by_project_name(request):
 class Post(APIView):
     permission_classes = (IsAdminOrReadOnly,)
 
-    def get_projects(self, pk):
+    def get_project(self, pk):
         try:
             return Post.objects.get(pk=pk)
         except Post.DoesNotExist:
@@ -98,3 +98,17 @@ class Post(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        project = self.get_project(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
