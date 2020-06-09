2c96200f81f59b17f39bd0bce31dd7131da7460a
Kvria <trishakuria@gmail.com>
2020-06-09 08:15:21 +0300

    add api url

--- projecter/project/urls.py
+++ projecter/project/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     url(r'^profile/', views.profile, name='profile'),
     url(r'^post/new', views.post_new, name= 'new_post'),
     url(r'^search/', views.search_users, name='search_users'),
+    url(r'^api/merch/$', views.MerchList.as_view())
 ]
